68c8ace47b31d6a682e9cc99301f7e4b07e7c243
Daniel Morris <daniel.morris@giganticplayground.com>
2025-12-01 12:07:47 -0600

    Cleanup generated files

--- packages/rule-loom-core/src/inputs/builtin.d.ts
+++
@@ -1,6 +0,0 @@
-import type { InputPlugin, BaseInputConfig } from './types.js';
-import { registerInputPlugin } from './pluginRegistry.js';
-export declare const builtinInputPlugins: InputPlugin<BaseInputConfig>[];
-export declare function registerBuiltinInputs(registerFn?: typeof registerInputPlugin): void;
-export default registerBuiltinInputs;
-//# sourceMappingURL=builtin.d.ts.map
--- packages/rule-loom-lib/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { LogLevel, RuleLoomLogger } from './logger.js';
-export { createLogger } from './logger.js';
-//# sourceMappingURL=index.d.ts.map
